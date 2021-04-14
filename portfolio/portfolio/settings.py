a1f15a49ee2d011a710ab3e3c5bc81d6123fa62a
AkarshSimha007 <akarsh07simha@gmail.com>
2021-04-14 07:46:30 +0000

    Remove old files

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@3-dy1-u__mi5k!a)^eh^+kx903kub(-drj@z4dyu7!an1ict4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simha007.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT =  BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
